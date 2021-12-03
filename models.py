4268b3b53a54bad71a619f88b94ac5005518a10a
NCTURN1 <36936831+NCTURN1@users.noreply.github.com>
2021-12-02 21:51:10 -0800

    Update models.py

--- models.py
+++ models.py
@@ -5,3 +5,16 @@ class NewMember(models.Model):
     product_name = models.CharField(max_length=200)
     product_price = models.FloatField()
     product_quantity = models.PositiveIntegerField()
+    
+ class MemberProfile(models. Model):
+    firstname = models.CharField("First name", max_length=255, blank = True, null = True)
+    lastName = models.CharField("Last name", max_length=255, blank = True, null = True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, blank = True, null = True)
+    address = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    
+    def __str__(self):
+        return self.firstName
+
